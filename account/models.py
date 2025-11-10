65aa26285853bca7b5fc5b3f04a34727b0607acb
Patrick Odeh <vyltyan@gmail.com>
2025-11-10 16:17:20 +0100

    added is_verified

--- account/models.py
+++ account/models.py
@@ -70,6 +70,7 @@ class User(AbstractBaseUser):
     tc = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_email_verify = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
